368c3656f6624a1e9af27d4e8a528f92aa063639
trickkyricky <rickygyoungjr@gmail.com>
2023-02-16 23:49:21 -0600

    comment

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../Root/App";
 import Smiley from "../Visualizations/Smiley/Smiley";
 
+// remember to put extra path here for / homepage
 const router = createBrowserRouter([
   {
     path: "/",
